dff475f32c0d13782ddff175ebe8194e5f61f513
olteanuflorin86 <olteanuflorin86@gmail.com>
2020-10-30 12:04:44 +0200

    First version of implementing views with a layout

--- core/Router.php
+++ core/Router.php
@@ -29,16 +29,37 @@ class Router
                 
         $callback = $this->routes[$method][$path] ?? false;
         if($callback === false) {
-            echo "Not found";
-            exit;
+            return "Not found";
         } 
+        if(is_string($callback)) {
+            return $this->renderView($callback);
+        }
         // if there is callback we need to execute this callback with call_user_func
         // The callback will return some string - because function() in index.php returns for ex Hello World
         // if we acces "/" (home page) we see Hello World
-        echo call_user_func($callback);
+        return call_user_func($callback);
+
+    }
+    
+    public function renderView($view) {
         
-
-
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+        
+    }
+    
+    protected function layoutContent() {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean();
+    }
+    
+    protected function renderOnlyView(string $view) {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean();
     }
 
     
--- public/index.php
+++ public/index.php
@@ -5,15 +5,15 @@
     // If we write the line below we can create an instance of Application in this file
     use app\core\Application;
     
-    $app = new Application();    
+    $app = new Application(dirname(__DIR__));    
     
     $app->router->get('/', function() {
         return 'Hello World';
     });
     
-    $app->router->get('/contact', function() {
-        return 'Hello World Contact';
-    });
+    $app->router->get('/contact', 'contact');
+    
+    $app->router->get('/home', 'home');
     
     $app->run();
     
