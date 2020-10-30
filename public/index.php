544bf294963b7c75c7c7a2105a3052a40059ae36
olteanuflorin86 <olteanuflorin86@gmail.com>
2020-10-30 09:39:28 +0200

    Add new file and folders to the current structure

--- public/index.php
+++ public/index.php
@@ -1,19 +1,18 @@
 <?php 
 
     require_once __DIR__.'/../vendor/autoload.php';
-
+    
     // If we write the line below we can create an instance of Application in this file
     use app\core\Application;
-
+    
     $app = new Application();    
-
     
     $app->router->get('/', function() {
         return 'Hello World';
     });
     
     $app->router->get('/contact', function() {
-        return 'Hello World contact';
+        return 'Hello World Contact';
     });
     
     $app->run();
